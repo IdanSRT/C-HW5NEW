8cb87b842b3d6009969f9764ec6c6cadb2a2f761
sagiGL <sagigleizer@gmail.com>
2016-06-11 18:20:32 +0300

    now ok

--- B16Ex05 SagiGleizer305746588 IdanShuraty/GameMenager.cs
+++ B16Ex05 SagiGleizer305746588 IdanShuraty/GameMenager.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Logic
+namespace UI
 {
     public enum eGameStatus
     {
