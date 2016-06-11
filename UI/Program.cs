e27a7245be8d14ee92b5ecc61e531f72aed50a89
Idan <Idanshuraty89@gmail.com>
2016-06-11 18:12:28 +0300

    Revert "no message"
    This reverts commit 359e81072a66d5291b4772a1a14b960297c45d03.

---
+++ UI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B16_Ex02_Idan_201580990_Sagi_305746588
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello!\nLet's play 4 in a row!");
+            int numOfPlayers = GameMenager.ChooseNumOf("Players", 1, 2);
+            int numOfRows = GameMenager.ChooseNumOf("Rows", 4, 8);
+            int numOfColumns = GameMenager.ChooseNumOf("Columns", 4, 8);
+            GameMenager NewGame = GameMenager.StartNewGame(numOfRows, numOfColumns, numOfPlayers);
+            NewGame.PlayGame();
+            Console.ReadLine();
+        }
+    }
+}
